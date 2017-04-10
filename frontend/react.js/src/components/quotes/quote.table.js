b288a31cb44e54d8a0ddc49df0496e12868400a0
Matt Pazaryna <mpazaryna@gmail.com>
2017-04-10 11:40:16 -0400

    Moved the css to an external file.

--- frontend/react.js/src/components/quotes/quote.table.js
+++ frontend/react.js/src/components/quotes/quote.table.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import { Panel } from "react-bootstrap";
+import "./table.css";
 
 const QuotesTable = ({ dats }) => {
   return (
-    <Panel header="Quotes" bsStyle="primary">
-      <BootstrapTable
-        data={dats}
-        tableStyle={{ border: "#0000FF 2.5px solid" }}
-        containerStyle={{ border: "#FFBB73 2.5px solid" }}
-        headerStyle={{ border: "red 1px solid" }}
-        bodyStyle={{ border: "green 1px solid" }}
-      >
-        <TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
-        <TableHeaderColumn
-          dataField="firstName"
-          dataSort={true}
-          filter={{ type: "TextFilter", delay: 1000 }}
+      <Panel header="Quotes" bsStyle="primary">
+        <BootstrapTable
+          data={dats}
+          containerClass='my-container-class'
+          headerClass='my-header-class'
+          bodyclass='my-body-class'
         >
-          First Name
-        </TableHeaderColumn>
-        <TableHeaderColumn
-          dataField="lastName"
-          dataSort={true}
-          filter={{ type: "TextFilter", delay: 1000 }}
-        >
-          Last Name
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="quote">Quote</TableHeaderColumn>
-      </BootstrapTable>
-    </Panel>
+          <TableHeaderColumn dataField="id" isKey={true}>ID</TableHeaderColumn>
+          <TableHeaderColumn
+            dataField="firstName"
+            dataSort={true}
+            filter={{ type: "TextFilter", delay: 1000 }}
+          >
+            First Name
+          </TableHeaderColumn>
+          <TableHeaderColumn
+            dataField="lastName"
+            dataSort={true}
+            filter={{ type: "TextFilter", delay: 1000 }}
+          >
+            Last Name
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="quote">Quote</TableHeaderColumn>
+        </BootstrapTable>
+      </Panel>
   );
 };
 
